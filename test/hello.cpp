8d6507d8d348d0b743f89c4b994b9624719b1555
rakshith_s <rakshithsantosh@gmail.com>
2025-04-30 10:27:26 +0530

    getting started witn cpp

---
+++ test/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//preprocessive directive
+
+using namespace std;
+//including namespace
+
+//main method with a return type
+int main (){
+    cout << "Hello world" << endl;
+    return 0;
+}
