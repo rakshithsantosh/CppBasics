e7b4d5ae981f39a92cb7043e6c91c354ed7e9d22
rakshith_s <rakshithsantosh@gmail.com>
2025-04-30 10:53:32 +0530

    variables and constants

---
+++ variables/variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //variable declaration
+    int score;
+
+    //variable initialization
+    score = 110;
+
+    //constant
+    const int uid = 1
+
+    cout << "this is about variables" << endl;
+    cout << score;
+    return 0;
+}
